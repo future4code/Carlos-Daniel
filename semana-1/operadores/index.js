27380eda36ce091ac5b8266b766ac39a246fdc91
Carlos Daniel Oliveira Nunes <carlosdaniel44u@gmail.com>
2021-08-11 16:11:47 -0300

    fiz os exercícios de strings e arrays

---
+++ semana-1/operadores/index.js
@@ -0,0 +1,42 @@
+/* 1) No primeiro console, o resultado será falso
+      No segundo console, também será false pois no conectivo e basta um false para que a sentença toda seja falsa 
+      No terceiro console, o resultado será verdadeiro
+      No quarto console, o resultado será */
+      
+/*2) O programa está imprimindo a junção dos dois números */
+
+/*3) Uma possível solução para o problema seria: atribuir valores para os números ao invés de juntar primeiroNumero + segundoNumero*/
+
+//Parte escrita - exercício 1
+
+/*let suaIdade = prompt("Qual é a sua idade?")
+let idadeMelhorAmigo = prompt("Qual é a idade do seu melhor amigo?")
+
+console.log ("Sua idade é maior que a do seu melhor amigo?" , suaIdade>idadeMelhorAmigo)
+console.log("Diferença de idade: " ,suaIdade-idadeMelhorAmigo)*/
+
+//Parte escrita - exercício 2
+
+/*let numeroPar = prompt("Insira um número par: ")
+console.log(numeroPar%2) */
+
+/* Padrão números pares: o mod sempre vai ser 0
+Caso o usuário insira um número ímpar, o % será diferente de 0 */
+
+//Parte escrita - exercício 3 
+
+/*let idadeUsuario = prompt("Digite sua idade: ")
+console.log(idadeUsuario*12)
+console.log(idadeUsuario*30)
+console.log(idadeUsuario*24) */
+
+//Parte escrita - exercício 4
+
+/*let primeiroNumero = Number(prompt("Primeiro número: "))
+let segundoNumero = Number(prompt("Segundo número: "))
+
+console.log("O primeiro numero é maior que segundo?" ,primeiroNumero>segundoNumero)
+console.log("O primeiro numero é igual ao segundo?" ,primeiroNumero===segundoNumero)
+console.log("O primeiro numero é divisível pelo segundo?" ,primeiroNumero%segundoNumero===0)
+console.log("O segundo numero é divisível pelo primeiro?" ,segundoNumero%primeiroNumero===0) */
+
---
+++ semana-1/semana1/strings-arrays/index.js
@@ -0,0 +1,33 @@
+// 1- irá aparecer na seguinte ordem: undefinded, null, 11, 3, 11 e 9
+
+//2 - A frase será: SUBI NUM ÔNIBUS EM MIRROCOS
+
+
+// parte escrita 
+/*1 - const nomeUsuario = prompt("Digite seu nome: ")
+const emailUsuario = prompt("Digite seu e-mail: ")
+
+console.log(`O e-mail ${emailUsuario} foi cadastrado com sucesso. Seja bem vindo(a) ${nomeUsuario}!`) */
+
+
+/* 2 - let comidasFavoritas= ["brócolis","batata","macarrão","ervilha","cenoura"]
+console.log(`${comidasFavoritas}`)
+
+let comidaFavoritaUsuario = prompt("Qual é sua comida preferida?")
+
+comidasFavoritas[1] = comidaFavoritaUsuario 
+console.log(comidasFavoritas) */
+
+/* 3 -let listaDeTarefas = []
+
+let primeiraTarefa = prompt("Digite a primeira tarefa aqui:")
+let segundaTarefa = prompt("Digite a segunda tarefa aqui:")
+let terceiraTarefa = prompt("Digite a terceira tarefa aqui: ")
+
+listaDeTarefas.push(primeiraTarefa)
+listaDeTarefas.push(segundaTarefa)
+listaDeTarefas.push(terceiraTarefa)
+
+let numeroDigitadoUsuario = Number(prompt("Digite o índice de uma tarefa já realizada"))
+listaDeTarefas.splice(numeroDigitadoUsuario,1)
+console.log(listaDeTarefas) */
