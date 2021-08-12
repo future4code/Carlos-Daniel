632a1508cdfb264ef0a97da1062b5b315b857e26
Carlos Daniel Oliveira Nunes <carlosdaniel44u@gmail.com>
2021-08-12 16:23:40 -0300

    Fiz os exercícios de funções

---
+++ semana1/funcoes/template_javascript/index.js
@@ -0,0 +1,52 @@
+// 1 a) Serão impresso os resultados das multiplicações por 5, ou seja, 10 e 50
+//   b) Seria impresso apenas o valor 50
+
+// 2) A função pede para o usuário inserir um texto. O texto será passado para letras minúsculas e receberá
+// true se possuir a palavra cenoura e false, caso contrário
+//  i) true
+//  ii) true
+//) iii) true 
+
+
+// PARTE ESCRITA 
+
+/* 1) function imprimirInformacoes(nome, idade, endereco, trabalho) {
+    const frase = (`Eu sou ${nome}, tenho ${idade} anos, moro em ${endereco} e sou ${trabalho}`) 
+    console.log(frase)
+}
+imprimirInformacoes("Carlos Daniel", 21, "Itumbiara", "estudante") */
+
+ 
+/* 2) 
+    a)function soma(primeiroNumero,segundoNumero) {
+    const somaEntradas = primeiroNumero+segundoNumero 
+    console.log(somaEntradas)
+}
+soma(5,7) */ 
+
+    
+/*  b) function boolean(primeiroNumero, segundoNumero) {
+const maiorOuIgual = primeiroNumero >= segundoNumero
+console.log(maiorOuIgual)
+}
+boolean(3,4) */
+
+ /* c) function numeroParOuNao(numero) {
+    const paridadeResultado = numero % 2 === 0
+    console.log(paridadeResultado)
+ }
+   numeroParOuNao(4) */
+
+  /* d) function mensagemString(mensagem) {
+       mensagem = "Você recebeu uma mensagem!".toUpperCase()
+       return mensagem
+   }
+   mensagemString() */
+
+
+   
+   
+   
+
+
+
