c831e063182aa0fd912ebd3e1b905ef6b49c4e68
Carlos Daniel Oliveira Nunes <carlosdaniel44u@gmail.com>
2021-09-09 23:05:33 -0300

    insta4

---
+++ semana5/insta/insta4/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+        <Post
+          nomeUsuario={'joao'}
+          fotoUsuario={'https://picsum.photos/1/50'}
+          fotoPost={'https://picsum.photos/201/150'}
+        />
+
+          <Post
+          nomeUsuario={'amanda'}
+          fotoUsuario={'https://picsum.photos/2/50'}
+          fotoPost={'https://picsum.photos/203/150'}
+        />
+      </MainContainer>
+      
+    );
+    
+  }
+
+  
+}
+
+
+export default App;
+
+
+
