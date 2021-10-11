e0a77fd014a67f3cab214653c9c192d0d3014514
Carlos Daniel Oliveira Nunes <carlosdaniel44u@gmail.com>
2021-10-11 16:39:22 -0300

    fiz as páginas e agora terminarei o layout

---
+++ semana10/labe-x/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      {(document.title = "LabeX")}
+      <h2>LabeX</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ semana10/labe-x/src/Pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>LabeX</h1>
+      <button>Ver Viagens</button>
+      <button>Área de Admin</button>
+    </div>
+  );
+}
