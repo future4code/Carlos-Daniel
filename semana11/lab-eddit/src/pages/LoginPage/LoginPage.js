42d89590fa6ff512b4bc22981be16071db1ad38b
Carlos Daniel Oliveira Nunes <carlosdaniel44u@gmail.com>
2021-10-27 15:34:47 -0300

    Formulários e Validação

--- semana11/lab-eddit/src/pages/LoginPage/LoginPage.js
+++ semana11/lab-eddit/src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { ScreenContainer, SignUpButtonContainer } from "./styled";
+import { useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import LoginForm from "./LoginForm";
+import { goToSignUpPage } from "../../routes/coordinator";
 
 const LoginPage = () => {
+  const history = useHistory();
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <ScreenContainer>
+      <h1>LabEddit</h1>
+      <LoginForm />
+
+      <SignUpButtonContainer>
+        <Button
+          onClick={() => goToSignUpPage(history)}
+          type={"submit"}
+          fullWidth
+          variant={"text"}
+          color={"primary"}
+        >
+          Não tem uma conta? Cadastre-se
+        </Button>
+      </SignUpButtonContainer>
+    </ScreenContainer>
   );
 };
 
