13ad1f5287470ba41983bf4a81395e6efd823b0b
Carlos Daniel Oliveira Nunes <carlosdaniel44u@gmail.com>
2021-10-31 23:29:00 -0300

    primeira parte da correção

--- semana11/lab-eddit/src/App.js
+++ semana11/lab-eddit/src/App.js
@@ -1,23 +1,11 @@
-import React, { useState } from "react";
-import theme from "./constants/theme";
-import Router from "./routes/Router";
-import { ThemeProvider } from "@material-ui/core/styles";
-import { BrowserRouter } from "react-router-dom";
-import Header from "./components/Header/Header";
+import React from "react";
+import Router from "./components/Router";
 
 const App = () => {
-  const token = localStorage.getItem("token");
-
-  const [rightButtonText, setRightButtonText] = useState(
-    token ? "logout" : "login"
-  );
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Header rightButtonText={rightButtonText} />
-        <Router rightButtonText={rightButtonText} />
-      </BrowserRouter>
-    </ThemeProvider>
+    <div>
+      <Router />
+    </div>
   );
 };
 
--- semana11/lab-eddit/src/constants/url.js
+++ semana11/lab-eddit/src/constants/url.js
@@ -1,1 +1,3 @@
-export const BASE_URL = " https://labeddit.herokuapp.com";
+import React from "react";
+
+export const BASE_URL = "https://labeddit.herokuapp.com";
