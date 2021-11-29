add0bddf5759c40faaa4a2cb1ebaabbee8504d51
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2021-11-29 16:30:43 -0400

    endpoint adicionar usuario

--- semana16/semana16-projeto/src/index.ts
+++ semana16/semana16-projeto/src/index.ts
@@ -9,41 +9,22 @@ app.use(express.json());
 app.use(cors());
 
 // 1 - Criar Usuário
-app.post("/user", async (req, res) => {
+app.post("/user", async (req: Request, res: Response) => {
   try {
-    await connection.raw(`
-    INSERT INTO User
-    (id,nickname, name, email)
-    VALUES(
-      ${Date.now().toString()},
-      "${req.body.nickname}",
-      "${req.body.name}",
-      "${req.body.email}",
-    )
-    `);
-    res.status(201).send("Usuário criado com sucesso!");
+    await connection("User").insert({
+      id: req.body.id,
+      nickname: req.body.nickname,
+      name: req.body.name,
+      email: req.body.email,
+    });
+    res.send("Usuário criado com suceso");
   } catch (error) {
-    res.status(500).send("Ocorreu algo errado! Por favor, tente novamente.");
+    res.status(500).send({ message: (error as Error).message });
   }
 });
-
-// 2 - Buscar usuário por id
-app.get("/user/:id", async (req, res) => {
-  try {
-    const resultado = await connection.raw(`
-    SELECT * FROM User
-    `);
-    res.status(200).send(resultado);
-  } catch (error) {
-    res.status(500).send("Ocorreu algo errado! Por favor, tente novamente.");
-  }
-});
-
-// 3 -Editar Usuários
-app.post("user/:id", async (req, res) => {});
 
 //Criação do Servidor
-const server = app.listen(process.env.PORT || 3003, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Server is running in http://localhost: ${address.port}`);
