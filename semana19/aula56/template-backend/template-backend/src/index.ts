608c2436766af7c1db1c7c92c23cd52f1d94aa2f
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2021-12-15 23:07:10 -0400

    editUser

--- semana19/aula56/template-backend/template-backend/src/index.ts
+++ semana19/aula56/template-backend/template-backend/src/index.ts
@@ -1,7 +1,6 @@
 import app from "./app";
 import createUser from "./endpoints/signUp";
 import login from "./endpoints/login";
-import { connection } from "./data/connection";
 
 app.post("/user/signup", createUser);
 app.post("/user/login", login);
---
+++ semana19/semana19-revisao/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Express } from "express";
+import knex from "knex";
+import cors from "cors";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+
+dotenv.config();
+
+export const connection = knex({
+  client: "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA,
+    port: 3306,
+    multipleStatements: true,
+  },
+});
